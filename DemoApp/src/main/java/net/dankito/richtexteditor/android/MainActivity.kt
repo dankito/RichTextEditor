71c00e576ecd6d95d2a72af23193c871af914129
dankl <dankl@comfylight.com>
2017-10-31 15:43:34 +0100

    Initial check in

---
+++ DemoApp/src/main/java/net/dankito/richtexteditor/android/MainActivity.kt
@@ -0,0 +1,13 @@
+package net.dankito.richtexteditor.android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
