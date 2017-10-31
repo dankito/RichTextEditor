dca7782090572a9bfad308df976063d4ee24ef1d
dankl <dankl@comfylight.com>
2017-10-31 17:56:58 +0100

    Implemented when pressing enter <p></p> gets inserted

--- RichTextEditorAndroid/src/main/assets/rich_text_editor.js
+++ RichTextEditorAndroid/src/main/assets/rich_text_editor.js
@@ -6,6 +6,23 @@ var editor = {
 
     init: function() {
         this.textField.addEventListener("input", this.textChangedCallback)
+
+        this.ensureEditorInsertsParagraphWhenPressingEnter()
+    },
+
+    ensureEditorInsertsParagraphWhenPressingEnter: function() {
+        // see https://stackoverflow.com/a/36373967
+        document.execCommand("DefaultParagraphSeparator", false, "p");
+
+        var newElement = document.createElement("p");
+        newElement.innerHTML = "&#8203";
+        this.textField.appendChild(newElement);
+
+        var selection=document.getSelection();
+        var range=document.createRange();
+        range.setStart(newElement.firstChild, 1);
+        selection.removeAllRanges();
+        selection.addRange(range);
     },
 
 
