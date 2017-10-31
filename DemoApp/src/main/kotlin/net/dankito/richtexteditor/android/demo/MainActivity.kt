f489464e9935c331cd4b8f44a1c86b47631ce921
dankl <dankl@comfylight.com>
2017-10-31 17:34:36 +0100

    Added RichTextEditor

--- DemoApp/src/main/kotlin/net/dankito/richtexteditor/android/demo/MainActivity.kt
+++ DemoApp/src/main/kotlin/net/dankito/richtexteditor/android/demo/MainActivity.kt
@@ -2,12 +2,19 @@ package net.dankito.richtexteditor.android.demo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import net.dankito.richtexteditor.android.RichTextEditor
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var editor: RichTextEditor
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editor = findViewById<RichTextEditor>(R.id.editor)
     }
+
 }
