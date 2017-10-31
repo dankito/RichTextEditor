3abf97e5c738e7c76fa02b70bdd14b267fe2a0dd
dankl <dankl@comfylight.com>
2017-10-31 21:36:09 +0100

    Added EditorToolbar

--- DemoApp/src/main/kotlin/net/dankito/richtexteditor/android/demo/MainActivity.kt
+++ DemoApp/src/main/kotlin/net/dankito/richtexteditor/android/demo/MainActivity.kt
@@ -3,11 +3,14 @@ package net.dankito.richtexteditor.android.demo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import net.dankito.richtexteditor.android.RichTextEditor
+import net.dankito.richtexteditor.android.toolbar.DefaultEditorToolbar
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var editor: RichTextEditor
+
+    private lateinit var editorToolbar: DefaultEditorToolbar
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         editor = findViewById<RichTextEditor>(R.id.editor)
+
+        editorToolbar = findViewById<DefaultEditorToolbar>(R.id.editorToolbar)
+        editorToolbar.editor = editor
     }
 
 }
