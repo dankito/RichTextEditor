c12257afd31c79ac842b7abc12d7dc2b0b5205fe
dankl <dankl@comfylight.com>
2017-11-03 14:03:37 +0100

    Implemented InsertLinkCommand and InsertImageCommand (forgot in previous commit)

---
+++ RichTextEditorAndroid/src/main/kotlin/net/dankito/richtexteditor/android/command/InsertImageCommand.kt
@@ -0,0 +1,21 @@
+package net.dankito.richtexteditor.android.command
+
+import android.support.v7.app.AppCompatActivity
+import net.dankito.richtexteditor.android.R
+import net.dankito.richtexteditor.android.RichTextEditor
+import net.dankito.richtexteditor.android.command.dialogs.EditImageDialog
+
+class InsertImageCommand : Command(Commands.INSERTIMAGE, R.drawable.ic_insert_photo_white_48dp) {
+
+
+    override fun executeCommand(editor: RichTextEditor) {
+        (editor.context as? AppCompatActivity)?.let { activity ->
+            val dialog = EditImageDialog()
+
+            dialog.show(activity.supportFragmentManager) { imageUrl, alternateText ->
+                editor.insertImage(imageUrl, alternateText)
+            }
+        }
+    }
+
+}
