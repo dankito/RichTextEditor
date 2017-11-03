3fde89a0902ea91b5856f306bc051530cc2b35a3
dankl <dankl@comfylight.com>
2017-11-03 17:20:45 +0100

    Extracted ColorCommand from SetColorCommand

---
+++ RichTextEditorAndroid/src/main/kotlin/net/dankito/richtexteditor/android/command/ColorCommand.kt
@@ -0,0 +1,51 @@
+package net.dankito.richtexteditor.android.command
+
+import android.graphics.Color
+import android.widget.ImageView
+import org.slf4j.LoggerFactory
+
+abstract class ColorCommand(defaultColor: Int, command: Commands, iconResourceId: Int, style: ToolbarCommandStyle = ToolbarCommandStyle(), commandExecutedListener: (() -> Unit)? = null)
+    : Command(command, iconResourceId, style, commandExecutedListener) {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(ColorCommand::class.java)
+    }
+
+
+    protected var currentColor: Int = defaultColor
+
+
+    override fun commandValueChanged(commandView: ImageView, commandValue: Any) {
+        super.commandValueChanged(commandView, commandValue)
+
+        if(commandValue is String) {
+            tryToParseColorFromString(commandValue)?.let { color ->
+                currentColorChanged(color)
+            }
+        }
+    }
+
+    private fun tryToParseColorFromString(colorString: String): Int? {
+        if(colorString.startsWith("rgba(") || colorString.startsWith("rgb(")) {
+            try {
+                val hexColorString = colorString.replace("rgba(", "").replace("rgb(", "").replace(")", "")
+                val hexValues = hexColorString.split(',').map { it.trim() }.filter { it.isNotEmpty() }.map { it.toIntOrNull() }.filterNotNull()
+
+                if (hexValues.size == 3) {
+                    return Color.rgb(hexValues[0], hexValues[1], hexValues[2])
+                }
+                else if (hexValues.size == 4) {
+                    return Color.argb(hexValues[3], hexValues[0], hexValues[1], hexValues[2])
+                }
+            } catch(e: Exception) { log.error("Could not parse color string $colorString", e) }
+        }
+
+        return null
+    }
+
+
+    protected open fun currentColorChanged(color: Int) {
+        this.currentColor = color
+    }
+
+}
