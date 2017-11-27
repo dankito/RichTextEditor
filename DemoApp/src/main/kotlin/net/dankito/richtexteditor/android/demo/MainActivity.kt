c15d65079c9f822a7f23cbfc2642e9a32c457426
dankl <dankl@comfylight.com>
2017-11-27 15:35:43 +0100

    Fixed casting views

--- DemoApp/src/main/kotlin/net/dankito/richtexteditor/android/demo/MainActivity.kt
+++ DemoApp/src/main/kotlin/net/dankito/richtexteditor/android/demo/MainActivity.kt
@@ -3,23 +3,23 @@ package net.dankito.richtexteditor.android.demo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import net.dankito.richtexteditor.android.RichTextEditor
-import net.dankito.richtexteditor.android.toolbar.DefaultEditorToolbar
+import net.dankito.richtexteditor.android.toolbar.AllCommandsEditorToolbar
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var editor: RichTextEditor
 
-    private lateinit var editorToolbar: DefaultEditorToolbar
+    private lateinit var editorToolbar: AllCommandsEditorToolbar
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editor = findViewById<RichTextEditor>(R.id.editor)
+        editor = findViewById(R.id.editor) as RichTextEditor
 
-        editorToolbar = findViewById<DefaultEditorToolbar>(R.id.editorToolbar)
+        editorToolbar = findViewById(R.id.editorToolbar) as AllCommandsEditorToolbar
         editorToolbar.editor = editor
     }
 
