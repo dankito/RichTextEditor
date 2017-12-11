d435d2dfe1938ccedb4f30e7a5e0e7db7d45cbc4
dankl <dankl@comfylight.com>
2017-12-11 10:02:22 +0100

    Fixed merge conflict

--- DemoApp/src/main/kotlin/net/dankito/richtexteditor/android/demo/MainActivity.kt
+++ DemoApp/src/main/kotlin/net/dankito/richtexteditor/android/demo/MainActivity.kt
@@ -1,6 +1,5 @@
 package net.dankito.richtexteditor.android.demo
 
-import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import net.dankito.richtexteditor.android.RichTextEditor
@@ -20,20 +19,8 @@ class MainActivity : AppCompatActivity() {
 
         editor = findViewById(R.id.editor) as RichTextEditor
 
-        editor.setEditorFontSize(20)
-        editor.setPadding((4 * resources.displayMetrics.density).toInt())
-
-        // a little bit experimental, uncomment these if you don't like them (and you most certainly won't)
-        editor.setEditorBackgroundColor(Color.YELLOW)
-        editor.setEditorFontColor(Color.MAGENTA)
-        editor.setEditorFontFamily("cursive")
-
         editorToolbar = findViewById(R.id.editorToolbar) as AllCommandsEditorToolbar
         editorToolbar.editor = editor
-
-        editor.postDelayed({
-            editor.focusEditorAndShowKeyboard()
-        }, 250)
     }
 
 }
