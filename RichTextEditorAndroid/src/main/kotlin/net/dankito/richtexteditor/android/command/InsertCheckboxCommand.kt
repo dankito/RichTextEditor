cb39b00a91cc7f29eee22908f31de4152f6ae8f4
dankl <dankl@comfylight.com>
2017-12-12 16:53:46 +0100

    Added ic_insert_checkbox, using it for InsertCheckboxCommand

--- RichTextEditorAndroid/src/main/kotlin/net/dankito/richtexteditor/android/command/InsertCheckboxCommand.kt
+++ RichTextEditorAndroid/src/main/kotlin/net/dankito/richtexteditor/android/command/InsertCheckboxCommand.kt
@@ -3,7 +3,7 @@ package net.dankito.richtexteditor.android.command
 import net.dankito.richtexteditor.android.R
 import net.dankito.richtexteditor.android.RichTextEditor
 
-class InsertCheckboxCommand(iconResourceId: Int = R.drawable.ic_add_white_48dp) : ToolbarCommand(Command.INSERTCHECKBOX, iconResourceId) {
+class InsertCheckboxCommand(iconResourceId: Int = R.drawable.ic_insert_checkbox) : ToolbarCommand(Command.INSERTCHECKBOX, iconResourceId) {
 
 
     override fun executeCommand(editor: RichTextEditor) {
