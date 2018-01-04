7fbc0684eaadda77216feed57fafc44f09a88d0f
dankl <dankl@comfylight.com>
2018-01-04 14:23:21 +0100

    Added playHorizontalAnimation()

--- RichTextEditorAndroid/src/main/kotlin/net/dankito/richtexteditor/android/animation/ShowHideViewAnimator.kt
+++ RichTextEditorAndroid/src/main/kotlin/net/dankito/richtexteditor/android/animation/ShowHideViewAnimator.kt
@@ -61,4 +61,40 @@ class ShowHideViewAnimator {
         animatorSet.start()
     }
 
+
+    fun playHorizontalAnimation(view: View, show: Boolean, xStart: Float, xEnd: Float, animationDurationMillis: Long = DefaultAnimationDurationMillis, animationEndListener: (() -> Unit)? = null) {
+        val xAnimator = ObjectAnimator
+                .ofFloat(view, View.X, xStart, xEnd)
+                .setDuration(animationDurationMillis)
+        xAnimator.interpolator = if(show) AccelerateInterpolator() else DecelerateInterpolator()
+
+        val alphaAnimator = ObjectAnimator
+                .ofFloat(view, View.ALPHA, if(show) 0f else 1f, if(show) 1f else 0f)
+                .setDuration(animationDurationMillis)
+        alphaAnimator.interpolator = if(show) AccelerateInterpolator() else DecelerateInterpolator()
+
+        val animatorSet = AnimatorSet()
+
+        xAnimator.addListener(object : Animator.AnimatorListener {
+
+            override fun onAnimationStart(animation: Animator) { }
+
+            override fun onAnimationRepeat(animation: Animator) { }
+
+            override fun onAnimationCancel(animation: Animator) { }
+
+            override fun onAnimationEnd(animation: Animator) {
+                if(show == false) {
+                    view.visibility = View.GONE
+                }
+
+                animationEndListener?.invoke()
+            }
+
+        })
+
+        animatorSet.playTogether(xAnimator, alphaAnimator)
+        animatorSet.start()
+    }
+
 }
