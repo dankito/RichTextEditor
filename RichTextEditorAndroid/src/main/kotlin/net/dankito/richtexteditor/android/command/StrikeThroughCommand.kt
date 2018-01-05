6e6f0bcc24df0a818d9834c195a7e51aa3ef01e4
dankl <dankl@comfylight.com>
2018-01-05 13:07:27 +0100

    Fixed removing icon

--- RichTextEditorAndroid/src/main/kotlin/net/dankito/richtexteditor/android/command/StrikeThroughCommand.kt
+++ RichTextEditorAndroid/src/main/kotlin/net/dankito/richtexteditor/android/command/StrikeThroughCommand.kt
@@ -1,7 +1,9 @@
 package net.dankito.richtexteditor.android.command
 
 import net.dankito.richtexteditor.Icon
+import net.dankito.richtexteditor.android.AndroidIcon
+import net.dankito.richtexteditor.android.R
 import net.dankito.richtexteditor.command.StrikeThroughCommandBase
 
 
-class StrikeThroughCommand(icon: Icon) : StrikeThroughCommandBase(icon)
+class StrikeThroughCommand(icon: Icon = AndroidIcon(R.drawable.ic_format_strikethrough_white_48dp)) : StrikeThroughCommandBase(icon)
