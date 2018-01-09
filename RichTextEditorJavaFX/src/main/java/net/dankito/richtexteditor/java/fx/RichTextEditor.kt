eb2ad3fe77d3ac42f82940b3734331ba14c8e92a
dankl <dankl@comfylight.com>
2018-01-09 23:06:56 +0100

    Added RichTextEditor

---
+++ RichTextEditorJavaFX/src/main/java/net/dankito/richtexteditor/java/fx/RichTextEditor.kt
@@ -0,0 +1,52 @@
+package net.dankito.richtexteditor.java.fx
+
+import javafx.scene.layout.HBox
+import javafx.scene.layout.Priority
+import javafx.scene.layout.Region
+import javafx.scene.web.WebView
+import tornadofx.*
+
+
+class RichTextEditor : HBox() {
+
+    private var webView = WebView()
+
+    val javaScriptExecutor = JavaFXJavaScriptExecutor(webView)
+
+
+    init {
+        setupHtmlEditor()
+    }
+
+
+
+    private fun setupHtmlEditor() {
+        minHeight = 200.0
+        prefHeight = Region.USE_COMPUTED_SIZE
+        useMaxHeight = true
+        webView.minHeight = 200.0
+        webView.prefHeight = Region.USE_COMPUTED_SIZE
+        webView.maxHeight = Double.MAX_VALUE
+
+        webView.isContextMenuEnabled = false
+
+        this.children.add(webView)
+        HBox.setHgrow(webView, Priority.ALWAYS)
+        webView.prefHeightProperty().bind(this.heightProperty())
+        webView.prefWidthProperty().bind(this.widthProperty())
+
+        isFillHeight = true
+    }
+
+
+
+    @JvmOverloads
+    fun focusEditor(alsoCallJavaScriptFocusFunction: Boolean = true) {
+        webView.requestFocus()
+
+        if(alsoCallJavaScriptFocusFunction) { // Calling focus() changes editor's state, this is not desirable in all circumstances
+            javaScriptExecutor.executeEditorJavaScriptFunction("focus()")
+        }
+    }
+
+}
