24fe46ab3c1cda723b4400485a0efbb586e6608b
dankl <dankl@comfylight.com>
2018-01-10 13:52:46 +0100

    Implemented EditUrlDialog

--- RichTextEditorJavaFX/src/main/java/net/dankito/richtexteditor/java/fx/command/InsertLinkCommand.kt
+++ RichTextEditorJavaFX/src/main/java/net/dankito/richtexteditor/java/fx/command/InsertLinkCommand.kt
@@ -3,21 +3,13 @@ package net.dankito.richtexteditor.java.fx.command
 import net.dankito.richtexteditor.Icon
 import net.dankito.richtexteditor.command.InsertLinkCommandBase
 import net.dankito.richtexteditor.java.fx.JavaFXIcon
+import net.dankito.richtexteditor.java.fx.command.dialogs.EditUrlDialog
 
 
 class InsertLinkCommand(icon: Icon = JavaFXIcon.fromResourceName("ic_insert_link_black_36dp.png")) : InsertLinkCommandBase(icon) {
 
-//    override var editor: RichTextEditor? = null
-
-
     override fun selectLinkToInsert(linkSelected: (url: String, title: String) -> Unit) {
-//        (editor?.context as? FragmentActivity)?.let { activity ->
-//            val dialog = EditUrlDialog()
-//
-//            dialog.show(activity.supportFragmentManager) { url, title ->
-//                linkSelected(url, title)
-//            }
-//        }
+        EditUrlDialog.show(linkSelected)
     }
 
 }
---
+++ RichTextEditorJavaFX/src/main/java/net/dankito/richtexteditor/java/fx/command/dialogs/EditUrlDialog.kt
@@ -0,0 +1,27 @@
+package net.dankito.richtexteditor.java.fx.command.dialogs
+
+import tornadofx.*
+
+
+class EditUrlDialog(val urlEnteredListener: (url: String, title: String) -> Unit)
+    : EnterTwoStringsDialogBase(FX.messages["dialog.edit.url.url.label"], FX.messages["dialog.edit.url.title.label"], FX.messages["dialog.edit.url.dialog.title"]) {
+
+    companion object {
+        fun show(urlEnteredListener: (url: String, title: String) -> Unit) {
+            EditUrlDialog(urlEnteredListener).show()
+        }
+    }
+
+
+    override fun enteringStringsDone(valueOne: String, valueTwo: String) {
+        val url = valueOne
+
+        var title = valueTwo
+        if(title.isEmpty()) {
+            title = url
+        }
+
+        urlEnteredListener.invoke(url, title)
+    }
+
+}
