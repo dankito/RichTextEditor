f1eb948fccb382e6c011cb0f7fe039bf573c5c51
dankl <dankl@comfylight.com>
2018-02-23 15:35:21 +0100

    Fixed converting HSV values to ColorAdjust

--- RichTextEditorJavaFX/src/main/java/net/dankito/richtexteditor/java/fx/extensions/ImageViewExtensions.kt
+++ RichTextEditorJavaFX/src/main/java/net/dankito/richtexteditor/java/fx/extensions/ImageViewExtensions.kt
@@ -19,9 +19,14 @@ fun ImageView.setImageTintColor(color: javafx.scene.paint.Color) {
 
 private fun ImageView.setImageTintColor(hsv: DoubleArray) {
     val colorAdjust = ColorAdjust()
-    colorAdjust.hue = hsv[0]
-    colorAdjust.saturation = hsv[1]
-    colorAdjust.brightness = hsv[2]
+
+    colorAdjust.hue = toColorAdjustValue(hsv[0]) // hsv values range from 0.0 to 1.0, ColorAdjust values range from -1.0 to 1.0
+    colorAdjust.saturation = toColorAdjustValue(hsv[1])
+    colorAdjust.brightness = toColorAdjustValue(hsv[2])
 
     this.effect = colorAdjust
+}
+
+private fun toColorAdjustValue(hsvValue: Double): Double {
+    return (hsvValue * 2) - 1.0
 }
