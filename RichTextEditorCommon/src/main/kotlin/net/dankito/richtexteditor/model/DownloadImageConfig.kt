4b3379295f67dcebbe74e5f6c7bc346ad3bef5f5
dankl <dankito@users.noreply.github.com>
2018-09-15 17:39:47 +0200

    Added @JvmOverloads so that Java code can profit from default arguments

--- RichTextEditorCommon/src/main/kotlin/net/dankito/richtexteditor/model/DownloadImageConfig.kt
+++ RichTextEditorCommon/src/main/kotlin/net/dankito/richtexteditor/model/DownloadImageConfig.kt
@@ -3,7 +3,7 @@ package net.dankito.richtexteditor.model
 import java.io.File
 
 
-open class DownloadImageConfig(val uiSetting: DownloadImageUiSetting,
+open class DownloadImageConfig @JvmOverloads constructor(val uiSetting: DownloadImageUiSetting,
                           val downloadFolderIfUserIsNowAllowedToSelectFolder: File? = null,
                           val predefinedDownloadFoldersUserCanChooseFrom: List<File>? = null,
                           val fileNameSelectorCallback: ((url: String) -> String)? = null)
